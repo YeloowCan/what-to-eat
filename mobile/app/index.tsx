a4f59aa2dc09403318b3f617a9fefdc0350786f2
YeloowCan <942789838@qq.com>
2026-01-04 16:25:35 +0800

    chore: 添加前端环境变量

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,24 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
+  // 测试环境变量读取
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        padding: 20,
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      {apiUrl && (
+        <Text style={{ marginTop: 20, fontSize: 12, color: "#666" }}>
+          API URL: {apiUrl}
+        </Text>
+      )}
     </View>
   );
 }
